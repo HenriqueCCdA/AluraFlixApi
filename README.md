7f2c6a09b398cfe45142b2b26dcde90551cedea1
Henrique Andrade <henrique.ccda@gmail.com>
2022-11-06 14:19:35 -0300

    Setup inicial do repo.

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
+
+## Preparando o ambiente de desenvolvimento
+
 ```console
 python -m venv .venv --upgrade-deps
 source .venv/bin/activate
-pip install -r requirements.txt
+pip install pip-tools
+pip-sync requirements.txt requirements-dev.txt
 ```
---
+++ aluraflix_api/core/tests/test_vidoes_read.py
@@ -0,0 +1,19 @@
+# from django.shortcuts import resolve_url
+# from rest_framework.test import APIClient
+# from rest_framework import status
+
+# client = APIClient
+
+# END_POINT = 'core:videos'
+
+# def test_list(client):
+
+#     url = resolve_url(END_POINT)
+
+#     resp = client.get(url)
+
+#     assert status.HTTP_200_OK == resp.status_code
+
+#     body = resp.json()
+
+#     assert
