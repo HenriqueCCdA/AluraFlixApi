af78640f6b0850067ca2bad6098879a530337908
Henrique Andrade <henrique.ccda@gmail.com>
2022-11-06 17:10:50 -0300

    Rotas para criar e listar videos #5.
    close #5

--- aluraflix_api/core/tests/conftest.py
+++ aluraflix_api/core/tests/conftest.py
@@ -1,18 +1,39 @@
 import pytest
 from faker import Faker
+from rest_framework.test import APIClient
 
 from aluraflix_api.core.models import Video
 
 fake = Faker()
 
 
+pytestmark = pytest.mark.django_db
+
+
 @pytest.fixture
 def video_info():
     return dict(
-        titulo=fake.name(), descricao=fake.sentence(nb_words=20), url="https://qualquer"
+        titulo=fake.name(), descricao=fake.sentence(nb_words=20), url=fake.url()
     )
 
 
 @pytest.fixture
-def video(video_info, db):
+def video(video_info):
     return Video.objects.create(**video_info)
+
+
+@pytest.fixture
+def list_videos():
+    list_ = [
+        Video(titulo=fake.name(), descricao=fake.sentence(nb_words=20), url=fake.url()),
+        Video(titulo=fake.name(), descricao=fake.sentence(nb_words=20), url=fake.url()),
+        Video(titulo=fake.name(), descricao=fake.sentence(nb_words=20), url=fake.url()),
+    ]
+    Video.objects.bulk_create(list_)
+
+    return list(Video.objects.all())
+
+
+@pytest.fixture
+def client():
+    return APIClient()
---
+++ aluraflix_api/core/tests/test_videos_list.py
@@ -0,0 +1,41 @@
+import pytest
+from django.shortcuts import resolve_url
+from rest_framework import status
+
+END_POINT = "core:videos-list-create"
+
+pytestmark = pytest.mark.django_db
+
+
+def test_list(client, list_videos):
+
+    url = resolve_url(END_POINT)
+
+    resp = client.get(url)
+
+    assert status.HTTP_200_OK == resp.status_code
+
+    body = resp.json()
+
+    assert len(list_videos) == len(body)
+
+    for from_db, from_api in zip(list_videos, body):
+        assert from_db.id == from_api["id"]
+        assert from_db.titulo == from_api["titulo"]
+        assert from_db.descricao == from_api["descricao"]
+        assert from_db.url == from_api["url"]
+
+
+def test_list_empty(client):
+
+    url = resolve_url(END_POINT)
+
+    resp = client.get(url)
+
+    assert status.HTTP_200_OK == resp.status_code
+
+    body = resp.json()
+
+    assert 0 == len(body)
+
+    assert [] == body
