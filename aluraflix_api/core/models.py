73441268aaa63d5da25e2770be52e6f78ecd9215
Henrique Andrade <henrique.ccda@gmail.com>
2022-11-13 00:07:24 -0300

    Modelo categoria add.

--- aluraflix_api/core/models.py
+++ aluraflix_api/core/models.py
@@ -10,11 +10,20 @@ class CreationModificationBase(models.Model):
         abstract = True
 
 
+class Categoria(CreationModificationBase):
+    titulo = models.CharField(max_length=60)
+    cor = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.titulo
+
+
 class Video(CreationModificationBase):
 
     titulo = models.CharField(max_length=60)
     descricao = models.TextField()
     url = models.URLField()
+    categoria = models.ForeignKey('Categoria', related_name='videos', on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.titulo
---
+++ aluraflix_api/core/tests/models/test_categoria_model.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+import pytest
+
+from aluraflix_api.core.models import Categoria
+
+pytestmark = pytest.mark.django_db
+
+
+def test_create(categoria):
+    assert Categoria.objects.exists()
+    assert categoria.id
+
+
+def test_str(categoria):
+    assert categoria.titulo == str(categoria)
+
+
+def test_create_and_modified_at(categoria):
+    assert isinstance(categoria.created_at, datetime)
+    assert isinstance(categoria.modified_at, datetime)
+
+
+def test_one_to_many(video, categoria):
+    assert categoria.videos.exists()
