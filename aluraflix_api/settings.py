66a5a64cde0a84f6ab8a08110b9188df72ea2762
Henrique Andrade <henrique.ccda@gmail.com>
2022-11-14 01:22:30 -0300

    Configurações.

--- aluraflix_api/settings.py
+++ aluraflix_api/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
+from decouple import config, Csv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a@*)-1kjlj=r*!5q&=lkc(+o)g&orf82!)wi1tc4=!$qbn7^0b'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
@@ -75,12 +78,9 @@ WSGI_APPLICATION = 'aluraflix_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+default_db_url = 'sqlite:///' + str(BASE_DIR / 'db.sqlite3')
+parse_db = dj_database_url.parse
+DATABASES = {'default': config('DATABASE_URL', default=default_db_url, cast=parse_db)}
 
 
 # Password validation
